fd60c2c704bce7717a691bd4ce849a9a261e7ed6
ishitasinha <ishiestar@gmail.com>
2017-08-03 13:38:21 +0530

    written logic

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var vowels = ["a", "e", "i", "o", "u"]
+// join an array of strings into a single template string:
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
 
+func shortenedName(fromName: String) -> String {
+    var fromName = fromName.lowercased()
+    for (char) in fromName.characters {
+        let char = String(char)
+        if(!vowels.contains(char)){
+            fromName.remove(at: fromName.startIndex)
+        } else {
+            break
+        }
+    }
+    return fromName
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    var lyricsTemplate = lyricsTemplate
+    lyricsTemplate = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+        .replacingOccurrences(of: "<SHORT_NAME>", with: shortenedName(fromName: fullName))
+    return lyricsTemplate
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+class ViewController: UIViewController {
+    @IBAction func displayLyrics(_ sender: Any) {
+        if(!(nameField.text?.isEmpty)!) {
+            lyricsText.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
+
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsText.text = ""
+    }
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
